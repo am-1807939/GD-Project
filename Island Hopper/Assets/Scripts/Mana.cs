f95a7217741631362d38b91767d6c4b1f06e96fc
lyth7 <am1807939@qu.edu.qa>
2022-04-15 17:02:16 +0300

    Mana refill every second

--- Island Hopper/Assets/Scripts/Mana.cs
+++ Island Hopper/Assets/Scripts/Mana.cs
@@ -9,7 +9,11 @@ public class Mana : MonoBehaviour
 	public float manaPoints = 100f;
 	public float maxManaPoints = 100f;	
 
+    public float manaRegenPoints = 1f;
+
     private Image mpBar;
+
+    private int nextUpdate=1;
 
 
     void Start()
@@ -23,6 +27,14 @@ public class Mana : MonoBehaviour
     void Update()
     {
         
+        if(Time.time>=nextUpdate){
+             nextUpdate=Mathf.FloorToInt(Time.time)+1;
+
+            if (manaPoints < maxManaPoints) {
+                manaPoints += manaRegenPoints;
+            }
+         }
+
         mpBar.fillAmount = manaPoints / maxManaPoints;
 
     }
@@ -30,7 +42,12 @@ public class Mana : MonoBehaviour
 
     public void ConsumeMana(float amount)
 	{	
-		manaPoints = manaPoints - amount;	
+        if (manaPoints > amount) {
+            manaPoints = manaPoints - amount;	
+
+        } else {
+            // not enough mana, show message
+        }
 	}
 	
 	public void RefillMana(float amount)
