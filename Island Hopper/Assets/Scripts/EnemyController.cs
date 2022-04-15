acb7ffbadaa1e6e2555ecefe6f18f4bb37bfe541
lyth7 <am1807939@qu.edu.qa>
2022-04-15 19:27:31 +0300

    Add slime chasing within detection radius

---
+++ Island Hopper/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    
+    public float speed = 20.0f;
+    public float lookRadius = 10f;
+
+	public float minDist = 1f;
+	public Transform target;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// if no target specified, assume the player
+		if (target == null) {
+
+			if (GameObject.FindWithTag ("Player")!=null)
+			{
+				target = GameObject.FindWithTag ("Player").GetComponent<Transform>();
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (target == null)
+			return;
+        
+        //get the distance between the chaser and the target
+		float distance = Vector3.Distance(transform.position,target.position);
+
+
+        if (distance <= lookRadius)
+		{
+		// face the target
+		transform.LookAt(target);
+
+		//so long as the chaser is farther away than the minimum distance, move towards it at rate speed.
+		if(distance > minDist)	
+			transform.position += transform.forward * speed * Time.deltaTime;	
+
+        }
+	}
+
+	// Set the target of the chaser
+	public void SetTarget(Transform newTarget)
+	{
+		target = newTarget;
+	}
+
+    void OnDrawGizmosSelected ()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, lookRadius);
+	}
+
+}
