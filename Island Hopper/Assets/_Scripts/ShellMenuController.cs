e7756b524d6457510fd4d93384f652bc99ce5062
Abdulla-ak1702167 <ak1702167@qu.edu.qa>
2022-05-05 03:31:31 +0300

    setting audio option

--- Island Hopper/Assets/_Scripts/ShellMenuController.cs
+++ Island Hopper/Assets/_Scripts/ShellMenuController.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ShellMenuController : MonoBehaviour
 {
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+    [SerializeField] private GameObject confirmationPrompt = null;
+
     [Header("Levels To Load")]
     public string _newGameLevel;
     private string levelToLoad;
@@ -36,5 +43,24 @@ public class ShellMenuController : MonoBehaviour
     public void ExitGameDialogYes()
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
     }
 }
